9d08db9737a5de5daea5f7691caf5949dbb681c1
Gustavo Lach <43425971+chompa111@users.noreply.github.com>
2018-12-05 02:52:53 -0200

    .
    modificações dos socketas

--- ESI_SOCKETA/socketa_MaisPop.py
+++ ESI_SOCKETA/socketa_MaisPop.py
@@ -14,15 +14,18 @@ def threadJob(con):
     myInterface = Interface()
     recebe = con.recv (1024)
     numero_de_resultados = recebe.decode()
+    print("numero de resultados:"+numero_de_resultados)
     lista_de_id = myInterface.devolveRecomendacaoPaginaInicial(int(numero_de_resultados))
-    string_de_ids = converter(lista_de_id)
-    con.send(string_de_ids.encode())
+    print("orignal do wesley:"+str(lista_de_id))
+    string_de_ids = str(lista_de_id)
+    print("respota:"+string_de_ids)
+    con.send((string_de_ids+"\n").encode())
 
 
 cont=0;
 while True :
     host = ''
-    port = 7103+cont
+    port = 7100+cont
     addr = (host, port)
     serv_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
     serv_socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
--- ESI_SOCKETA/socketa_das_sugestoes.py
+++ ESI_SOCKETA/socketa_das_sugestoes.py
@@ -1,7 +1,39 @@
 import socket
-from time import *
-from decimal import Decimal
+from time import*
 from threading import Thread
 from Interface import Interface
-from Busca import Busca
 
+
+def converter(lista_de_id):
+    string_de_ids = ""
+    for id in lista_de_id:
+        string_de_ids = string_de_ids + "," + id
+    return string_de_ids
+
+def threadJob(con):
+    myInterface = Interface()
+    recebe = con.recv (1024)
+    stringincomleta = recebe.decode('latin_1')
+    print("sugestão solicitada:"+stringincomleta)
+    resposta=myInterface.retorne_sugestoes(stringincomleta)
+    print("orignal do wesley:"+str(resposta))
+    respostaFinal=str(resposta)
+    con.send((respostaFinal+"\n").encode())
+
+
+cont=0;
+while True :
+    host = ''
+    port = 7300+cont
+    addr = (host, port)
+    serv_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    serv_socket.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serv_socket.bind (addr)
+    serv_socket.listen (0)
+    con, cliente = serv_socket.accept ()
+    serv_socket.close ()
+    cont+=1
+    if cont==11:
+        cont=0
+    Thread (target=threadJob, args=[con]).start()
+    print("thread DELEGADA")
